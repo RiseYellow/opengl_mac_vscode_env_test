901959d51a134fe3d8450ab653f5b1ef753d0188
riseyellow <riseyellow@riseyellowdeMBP.lan>
2020-12-02 23:24:11 +0800

    first commit

---
+++ build/CMakeFiles/opengl_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_test.dir/src/main.cpp.o"
+  "opengl_test"
+  "opengl_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
